1b8c48215ec6767cb0e33318d54c9dad114a6427
Калинин Михаил Дмитриевич <42715865+mwaterlol@users.noreply.github.com>
2022-10-11 17:26:31 +0300

    Add files via upload

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import styles from './mystyle.module.css'
 import { useRef, useState } from "react"
 import { CSSTransition } from 'react-transition-group';
 import {Link} from 'react-scroll'
+import { CompanyList } from './CompanyList';
 
 
 export function Navbar(){
@@ -24,68 +25,13 @@ export function Navbar(){
             <img className={styles.e_vector} src={require("./templates/e.png")}></img>
             </div>
             <div className={styles.menu}>
-                <a className={styles.link_1} href="#"><Link to="calculator" spy={true} smooth={true}>Трекинг контейнеров</Link></a>
+                <a className={styles.link_1} href="#"><Link to="traker" spy={true} smooth={true}>Трекинг контейнеров</Link></a>
                 <a className={styles.link_2} href="#" 
-                onClick={() => {setDetails(prev => !prev)}}>
+                onClick={() => {setDetails(details => !details)}}>
                     Перевозчики
                 </a>
                 <CSSTransition in={details} classNames='alert' timeout={300} unmountOnExit>
-                    <div className={styles.list_full}>
-                        <div className={styles.list_full_body}>
-                            <div className={styles.list_full_title_and_icon}>
-                                <div className={styles.list_full_title}>
-                                Сейчас наш сервис позволяет производить трекинг<br /> контейнеров следующих компаний:
-                                </div>
-                                <button  onClick={() => setDetails(prev => !prev)}><img className={styles.list_full_icon_close} src={require("./templates/icon_close.png")}></img></button>
-                            </div>
-                            <div className={styles.list_full_list}>
-                                <div className={styles.list_full_column_1}>
-                                    <ul style={{listStyle: "none"}}>
-                                        <li>Admiral Container Lines</li>
-                                        <li>Alianca</li>
-                                        <li>Avana Global FZCO (BALAJI)</li>
-                                        <li>BLPL Singapore, Blue Water Lines</li>
-                                        <li>BMC Line Shipping</li>
-                                        <li>CONCOR (Container Corporation<br /> of India Ltd)</li>
-                                        <li>CMA CGM Group</li>
-                                        <li>China United Lines</li>
-                                        <li>COSCO</li>
-                                        <li>Crowley Maritime</li>
-                                    </ul>
-                                </div>
-                                <div className={styles.list_full_column_2}>
-                                    <ul style={{listStyle: "none"}}>
-                                        <li>Dongjin Shipping</li>
-                                        <li>Deutsche Afrika-Linien, Dong Young Shipping</li>
-                                        <li>Econship</li>
-                                        <li>ECU Worldwide</li>
-                                        <li>Emirates Shipping Line</li>
-                                        <li>Emkay Lines</li>
-                                        <li>Ethiopian Shipping Line</li>
-                                        <li>Evergreen</li>
-                                        <li>FESCO</li>
-                                        <li>Gold Star</li>
-                                    </ul>
-                                    
-                                </div>
-                                <div className={styles.list_full_column_3}>
-                                    <ul style={{listStyle: "none"}}>
-                                    <li>Dongjin Shipping</li>
-                                        <li>Deutsche Afrika-Linien, Dong Young Shipping</li>
-                                        <li>Econship</li>
-                                        <li>ECU Worldwide</li>
-                                        <li>Emirates Shipping Line</li>
-                                        <li>Emkay Lines</li>
-                                        <li>Ethiopian Shipping Line</li>
-                                        <li>Evergreen</li>
-                                        <li>FESCO</li>
-                                        <li>Gold Star</li>
-                                    </ul>
-                                </div>
-                            </div>  
-
-                        </div>
-                    </div>
+                <CompanyList changeSnowInfo={(details: boolean | ((prevState: boolean) => boolean)) => setDetails(details)} />
                 </CSSTransition>
                 <a className={styles.link_3} href="#">Калькулятор доставки</a>
                 <a className={styles.link_4} href="#">Трекинг посылок</a>
